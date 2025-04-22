8cf5937113578c9493809bb0de4d9759c286c4f2
ereninpenceresi <erenekerr47@gmail.com>
2025-04-22 04:07:51 +0300

    Yükleme Ekranı

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
@@ -7,24 +10,41 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: SizedBox.expand(
         child: Column(
           children: [
             // Logo bölümü
             Expanded(
               child: Container(
-                width: 150,
-                height: 150,
+                width: 400,
+                height: 400,
                 child: Image.asset(
-                  'assets/images/logo.png',
+                  'assets/images/gym.webp',
                   fit: BoxFit.contain,
                 ),
               ),
             ),
 
-            // Yükleniyor yazısı
-            CircularProgressIndicator(),
+            InkWell(
+              onTap: () => context.go("/home"),
+              child: SizedBox(
+                width: 250,
+                height: 250,
+                child: DotLottieLoader.fromAsset(
+                  "assets/motions/weightlifting.lottie",
+                  frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+                    if (dotlottie != null) {
+                      return Lottie.memory(dotlottie.animations.values.single);
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
           ],
         ),
       ),
