87385b4f549720123ab3eeab135f8de750060776
ereninpenceresi <erenekerr47@gmail.com>
2025-05-18 06:43:45 +0300

    Sistem güncellendi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.flutter_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "25.1.8937393"
+    compileSdk = 33
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
